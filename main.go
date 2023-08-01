92149975f8b202d0dc716d74b7591087848b5152
yoqub-davlatov <yoqub.dj03@gmail.com>
2023-08-01 15:28:01 +0500

    emptied the main.go

--- main.go
+++ main.go
@@ -1,17 +1,5 @@
 package main
 
-import (
-	"example/main/list"
-	"fmt"
-)
-
-func print(list *list.List) {
-	for it := list.Front(); it != nil; it = it.Next() {
-		fmt.Printf("%v ", it.Val)
-	}
-	fmt.Println()
-}
-
 func main() {
 	
 }
