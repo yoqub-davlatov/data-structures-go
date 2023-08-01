8df6e6a7c47904947a6a08379a9d2e11b1cd012a
yoqub-davlatov <yoqub.dj03@gmail.com>
2023-08-01 15:26:58 +0500

    emptied the main.go

--- main.go
+++ main.go
@@ -13,26 +13,5 @@ func print(list *list.List) {
 }
 
 func main() {
-	list := list.ListInit()
-	it1 := list.PushBack(7)
-	it2 := list.PushFront(4)
-	it3 := list.PushBack(8)
-	it4 := list.PushFront(2)
-	print(list)
-	i := 1
-	j := -1
-	for it := list.Front(); it != nil; it = it.Next() {
-		if it.Val == 8 {
-			list.InsertAfter(j, it)
-			list.InsertBefore(i, it)
-		}
-	}
-	print(list)
-	list.MoveAfter(it1, it3)
-	print(list)
-	list.MoveBefore(it2, it4)
-	print(list)
-	list.PushFront(-10)
-	list.PushBack(10)
-	print(list)
+	
 }
